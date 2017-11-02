8cd492e4c0e85997521612c34c53da2dd6b89efc
GAGAN-PIVOTAL <gaganpivotal@gmail.com>
2017-11-02 16:19:02 +0530

    remove unnecessary code

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -7,15 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
-    //@Value("${WELCOME_MESSAGE}")
+    
     private String message;
 
-    /*@Autowired
-    Message message;
-    WelcomeController(Message message){
-        //message.setMessage(welcomeMessage);
-        welcomeMessage = message.getMessage();
-    }*/
+
 
     public WelcomeController(@Value("${WELCOME_MESSAGE}") String message){
         this.message = message;
